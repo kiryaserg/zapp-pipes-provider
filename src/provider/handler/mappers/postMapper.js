6ff3b22a0403ca51e2af97d0a25e02301f13f17f
Yaniv Solnik <solnik@gmail.com>
2017-10-26 13:11:43 +0300

    more readability corrections

--- src/provider/handler/mappers/postMapper.js
+++ src/provider/handler/mappers/postMapper.js
@@ -1,49 +1,44 @@
 export function mapPost(mediaItems) {
   return function(post) {
-    const { id, date: publish, link, title } = post;
+    const {
+      id,
+      date: publish,
+      link,
+      title: { rendered: title },
+      content: { rendered: html }
+    } = post;
 
     //if we can find the post's media id then let's add its url to our media_group
-    const media_group = mediaItems.reduce((result, mediaItem) => {
-      if (
-        mediaItem &&
-        mediaItem.id === post.featured_media &&
-        result.length === 0
-      ) {
+    const media_group = mediaItems.find(mediaItem => {
+      if (mediaItem && mediaItem.id === post.featured_media) {
+        const { image: src } = mediaItem;
         return [
           {
             type: 'image',
             media_item: [
               {
-                src: mediaItem.image,
+                src,
                 key: 'image_base'
               }
             ]
           }
         ];
       }
-      return result;
-    }, []);
-
-    //adding the post's html content
-    const content = post.content.rendered
-      ? {
-          html: post.content.rendered
-        }
-      : null;
+    });
 
     return {
       type: {
         value: 'article'
       },
       id,
-      title: title.rendered,
+      title,
       publish,
       media_group,
-      content,
+      content: { html },
       link: {
         type: 'text/html',
         rel: 'alternate',
-        href: post.link //the post's link that will be used when a user shares the post
+        href: link //the post's link that will be used when a user shares the post
       }
     };
   };
